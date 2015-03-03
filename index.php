e4055c5754c5f42127acc041d56e482aeeb8344e
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-03 09:51:44 +0630

    Update
    Formatting

--- index.php
+++ index.php
@@ -3,17 +3,15 @@
  // Display Errors On
 ini_set('display_errors', 'On');
 
+use SetKyar\MoneyFormat\MoneyFormat;
+
 $autoload = __DIR__.'/vendor/autoload.php';
 
-if ( ! file_exists($autoload))
-{
+if (!file_exists($autoload)) {
 	exit("Need to run \"composer install\" for example");
 }
 
 require $autoload;
-
-
-use SetKyar\MoneyFormat\MoneyFormat;
 
 $currency = new MoneyFormat;
 var_dump($currency->Format(2000, 'Kyats'));
